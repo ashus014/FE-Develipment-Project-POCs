e5cad3f9147e8e6410b1c87976c06761b5e223b8
Ashu Singh <ashu.ask.singh@gmail.com>
2022-02-06 22:22:19 +0530

    more with JQuery

--- jQuery/index.js
+++ jQuery/index.js
@@ -3,3 +3,25 @@
 //$("h1").addClass("big-title");
 
 //$("button").html("<em>Hey</em>");
+
+//$("a").attr("href","https://www.yahoo.com");
+
+$("h1").click(function() {
+    $("h1").css("color","purple");
+});
+
+$("button").click(function(){
+    $("h1").css("color","purple");
+});
+
+// $("input").keypress(function(){
+//     console.log(event.key);
+// });
+
+// $(document).keypress(function(){
+//     console.log(event.key);
+// });
+
+$(document).keypress(function(){
+    $("h1").text(event.key);
+});
