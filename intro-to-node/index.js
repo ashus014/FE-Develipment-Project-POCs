acff2acc6397661a4e26e600077972cec60f9720
Ashu Singh <ashu.ask.singh@gmail.com>
2022-02-12 01:10:55 +0530

    SuperHero VS SuperVillain

--- intro-to-node/index.js
+++ intro-to-node/index.js
@@ -1,5 +1,10 @@
 //jshint esversion:6
 
-const fs = require("fs");
+var superheroes = require("superheroes");
+const supervillains = require("supervillains");
 
-fs.copyFileSync("file1.txt","file2.txt")
+var mySuperheroName = superheroes.random();
+var mySupervillainName = supervillains.random();
+
+
+console.log(mySuperheroName + "  VS  " + mySupervillainName);
