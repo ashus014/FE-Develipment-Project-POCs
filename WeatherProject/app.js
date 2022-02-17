ce36e91e24814a8c5cda3a65bffa432cca9ca543
Ashu Singh <ashu.ask.singh@gmail.com>
2022-02-17 23:55:34 +0530

    Adding Image to Weather App

--- WeatherProject/app.js
+++ WeatherProject/app.js
@@ -20,8 +20,14 @@ app.get("/", function(req, res){
             const temp = weatherData.main.temp;
             console.log(temp);
 
+            const icon = weatherData.weather[0].icon;
+
+            const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+
             res.write("<p>Hi, this is Ashu Singh Weather Forecast<p>");
             res.write("<h1>The temperature in Chennai is " + temp + " degrees Celcius</h1>");
+            res.write("<img src=" + imageURL + ">");
+            
             res.send();
         });
     });
