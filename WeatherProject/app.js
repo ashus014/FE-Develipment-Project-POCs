a020b783b9b23b9c4e935b9ff34d2c7859ceebc1
Ashu Singh <ashu.ask.singh@gmail.com>
2022-02-18 00:40:35 +0530

    Using Form input with get & Post

--- WeatherProject/app.js
+++ WeatherProject/app.js
@@ -1,12 +1,23 @@
 const { response } = require("express");
 const express = require("express");
 const https = require("https");
+const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.get("/", function(req, res){
 
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=Chennai&appid=bffebb15db4f0320ddf98168f18e79a9&units=metric";
+    res.sendFile(__dirname + "/index.html");   
+});
+
+app.post("/", function(req, res){
+
+    const query = req.body.cityName;
+    const apiKey = "bffebb15db4f0320ddf98168f18e79a9";
+    const unit= "metric";
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apiKey + "&units=" + unit;
     
     https.get(url, function(response){
         console.log(response.statusCode);
@@ -25,12 +36,13 @@ app.get("/", function(req, res){
             const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
 
             res.write("<p>Hi, this is Ashu Singh Weather Forecast<p>");
-            res.write("<h1>The temperature in Chennai is " + temp + " degrees Celcius</h1>");
+            res.write("<h1>The temperature in " + query + " is " + temp + " degrees Celcius</h1>");
             res.write("<img src=" + imageURL + ">");
             
             res.send();
         });
     });
+
 });
 
 app.listen(3000, function(){
