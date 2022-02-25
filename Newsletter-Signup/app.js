0b6724c7e146b6d463b3d0b8f1cc47863fb16839
Ashu Singh <ashu.ask.singh@gmail.com>
2022-02-25 22:44:00 +0530

    Complete Chapter 1

--- Newsletter-Signup/app.js
+++ Newsletter-Signup/app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
+const https = require("https");
+const { options } = require("request");
 
 const app = express();
 
@@ -14,14 +16,48 @@ app.get("/", function(req, res){
 
 app.post("/", function(req, res){
 
-    var firstName = req.body.fName;
-    var lastName = req.body.lName;
-    var email = req.body.email;
+    const firstName = req.body.fName;
+    const lastName = req.body.lName;
+    const email = req.body.email;
 
-    console.log(firstName, lastName, email);
+    const data = {
+        members: [
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME: lastName
+                }
+            }
+        ]
+    };
+
+    const jsonData = JSON.stringify(data);
+
+    const url = "https://us14.api.mailchimp.com/3.0/lists/951c12adf9";
+    const options = {
+
+        method: "POST",
+        auth: "ashu1:8ccc288f469f68faa8d605ddc5e4d721-us14"
+    };
+
+    const request = https.request(url, options, function(response){
+
+        response.on("data", function(){
+            console.log(JSON.parse(data));
+        });
+    });
+
+    request.write(jsonData);
+    request.end();
+
 });
 
 app.listen(3000, function(){
 
     console.log("Server started at port 3000");
 });
+
+//8ccc288f469f68faa8d605ddc5e4d721-us14
+//951c12adf9
